741c35c237c7cb8f2b144f971f018c0ca37f1441
Alex Wu <alexanderwu@berkeley.edu>
2019-11-09 02:03:50 -0800

    Updated comments on syscall numbers

--- pintos/src/lib/syscall-nr.h
+++ pintos/src/lib/syscall-nr.h
@@ -4,7 +4,7 @@
 /* System call numbers. */
 enum
   {
-    /* Projects 2 and later. */
+    /* Projects 1 and later. */
     SYS_HALT,                   /* Halt the operating system. */
     SYS_EXIT,                   /* Terminate this process. */
     SYS_EXEC,                   /* Start another process. */
@@ -20,11 +20,11 @@ enum
     SYS_CLOSE,                  /* Close a file. */
     SYS_PRACTICE,               /* Returns arg incremented by 1 */
 
-    /* Project 3 and optionally project 4. */
+    /* Unused. */
     SYS_MMAP,                   /* Map a file into memory. */
     SYS_MUNMAP,                 /* Remove a memory mapping. */
 
-    /* Project 4 only. */
+    /* Project 3 only. */
     SYS_CHDIR,                  /* Change the current directory. */
     SYS_MKDIR,                  /* Create a directory. */
     SYS_READDIR,                /* Reads a directory entry. */
